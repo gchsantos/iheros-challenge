57a20fe9e705c69ea5b1a5611a1b50dd65a24e09
gchsantos <gchsantos@gmail.com>
2022-07-25 14:03:54 -0300

    feat: create credential management

--- iheros_api/iheros_api/settings.py
+++ iheros_api/iheros_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%kd6co3kv(3%m)&h4)tu^g1dp!f2n$g#1lanf_o0v=($w$-nf'
+SECRET_KEY = 'django-insecure-n%kd6co3kv(3%m)&h4)tu^g1dp!f2n$g#1lanf_o0v=($w$-nf' # TODO: esconder secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
