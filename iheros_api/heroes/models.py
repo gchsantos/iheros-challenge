05aa1f87d9dd5dcdf3b724fb265899f127e2956c
gchsantos <gchsantos@gmail.com>
2022-08-03 14:08:07 -0300

    feat(heroes): create Identity model and migration
    - create vscode settings json to maintain development standards

--- iheros_api/heroes/models.py
+++ iheros_api/heroes/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Identity(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    classification = models.ForeignKey(
+        "Classes", on_delete=models.CASCADE, related_name="heroclassification"
+    )
+    lat = models.FloatField("Latitude", blank=True)
+    lng = models.FloatField("Longitude", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Classes(models.Model):
+    classification = models.TextField(unique=True)
